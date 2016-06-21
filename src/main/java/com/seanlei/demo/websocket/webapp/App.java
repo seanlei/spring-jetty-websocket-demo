2f00aa32253230783007c6849fb7f24607c3a3bf
sean lei <sean.yu.lei@gmail.com>
2016-06-21 12:26:59 +0800

    add detail config before start jetty server

--- src/main/java/com/seanlei/demo/websocket/webapp/App.java
+++ src/main/java/com/seanlei/demo/websocket/webapp/App.java
@@ -2,7 +2,15 @@ package com.seanlei.demo.websocket.webapp;
 
 import com.seanlei.demo.websocket.config.AppConfig;
 import com.seanlei.demo.websocket.config.WebsocketConfig;
+import org.apache.tomcat.InstanceManager;
+import org.apache.tomcat.SimpleInstanceManager;
+import org.eclipse.jetty.annotations.ServletContainerInitializersStarter;
+import org.eclipse.jetty.apache.jsp.JettyJasperInitializer;
+import org.eclipse.jetty.jsp.JettyJspServlet;
+import org.eclipse.jetty.plus.annotation.ContainerInitializer;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.Configuration;
 import org.eclipse.jetty.webapp.WebAppContext;
@@ -14,8 +22,16 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.EventListener;
+import java.util.List;
 
 /**
  * Created by Sean Lei on 6/20/16.
@@ -28,7 +44,6 @@ public class App {
     public void start(WebApplicationContext webCxt) {
         WebAppContext contextHandler = new WebAppContext();
 
-//        ServletContextHandler contextHandler = new ServletContextHandler();
         contextHandler.setContextPath("/");
 
         DispatcherServlet dispatcherServlet = new DispatcherServlet(webCxt);
@@ -66,6 +81,127 @@ public class App {
         webCxt.setParent(context);
 
         App app = context.getBean(App.class);
-        app.start(webCxt);
+        try {
+            app.startA(webCxt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    public void startA(WebApplicationContext webCxt) throws Exception {
+        Server server = new Server();
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(serverPort);
+
+        server.addConnector(connector);
+
+        URI baseUri = getWebRootResourceUri();
+
+        // Set JSP to use Standard JavaC always
+        System.setProperty("org.apache.jasper.compiler.disablejsr199", "false");
+
+        WebAppContext webAppContext = getWebAppContext(baseUri, getScratchDir());
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webCxt);
+        webAppContext.addServlet(new ServletHolder(dispatcherServlet), "/");
+        webAppContext.setEventListeners(new EventListener[]{new ContextLoaderListener(webCxt)});
+        server.setHandler(webAppContext);
+
+        // Start Server
+        server.start();
+    }
+
+    private URI getWebRootResourceUri() throws FileNotFoundException, URISyntaxException {
+        URL indexUri = this.getClass().getResource("/webapp");
+        if (indexUri == null) {
+            throw new FileNotFoundException("Unable to find resource " + "webapp");
+        }
+        // Points to wherever /webroot/ (the resource) is
+        return indexUri.toURI();
+    }
+
+    /**
+     * Establish Scratch directory for the servlet context (used by JSP compilation)
+     */
+    private File getScratchDir() throws IOException {
+        File tempDir = new File(System.getProperty("java.io.tmpdir"));
+        File scratchDir = new File(tempDir.toString(), "embedded-jetty-jsp");
+
+        if (!scratchDir.exists()) {
+            if (!scratchDir.mkdirs()) {
+                throw new IOException("Unable to create scratch directory: " + scratchDir);
+            }
+        }
+        return scratchDir;
+    }
+
+    /**
+     * Setup the basic application "context" for this application at "/"
+     * This is also known as the handler tree (in jetty speak)
+     */
+    private WebAppContext getWebAppContext(URI baseUri, File scratchDir) {
+        WebAppContext context = new WebAppContext();
+        context.setContextPath("/");
+        context.setAttribute("javax.servlet.context.tempdir", scratchDir);
+        context.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+                ".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$|.*/.*taglibs.*\\.jar$");
+        context.setResourceBase(baseUri.toASCIIString());
+        context.setAttribute("org.eclipse.jetty.containerInitializers", jspInitializers());
+        context.setAttribute(InstanceManager.class.getName(), new SimpleInstanceManager());
+        context.addBean(new ServletContainerInitializersStarter(context), true);
+        context.setClassLoader(getUrlClassLoader());
+
+        context.addServlet(jspServletHolder(), "*.jsp");
+
+        context.addServlet(defaultServletHolder(baseUri), "/");
+        return context;
+    }
+
+    /**
+     * Ensure the jsp engine is initialized correctly
+     */
+    private List<ContainerInitializer> jspInitializers() {
+        JettyJasperInitializer sci = new JettyJasperInitializer();
+        ContainerInitializer initializer = new ContainerInitializer(sci, null);
+        List<ContainerInitializer> initializers = new ArrayList<ContainerInitializer>();
+        initializers.add(initializer);
+        return initializers;
+    }
+
+    /**
+     * Set Classloader of Context to be sane (needed for JSTL)
+     * JSP requires a non-System classloader, this simply wraps the
+     * embedded System classloader in a way that makes it suitable
+     * for JSP to use
+     */
+    private ClassLoader getUrlClassLoader() {
+        ClassLoader jspClassLoader = new URLClassLoader(new URL[0], this.getClass().getClassLoader());
+        return jspClassLoader;
+    }
+
+    /**
+     * Create JSP Servlet (must be named "jsp")
+     */
+    private ServletHolder jspServletHolder() {
+        ServletHolder holderJsp = new ServletHolder("jsp", JettyJspServlet.class);
+        holderJsp.setInitOrder(0);
+        holderJsp.setInitParameter("logVerbosityLevel", "DEBUG");
+        holderJsp.setInitParameter("fork", "false");
+        holderJsp.setInitParameter("xpoweredBy", "false");
+//        holderJsp.setInitParameter("compilerTargetVM", "1.7");
+//        holderJsp.setInitParameter("compilerSourceVM", "1.7");
+        holderJsp.setInitParameter("keepgenerated", "true");
+        return holderJsp;
+    }
+
+    /**
+     * Create Default Servlet (must be named "default")
+     */
+    private ServletHolder defaultServletHolder(URI baseUri) {
+        ServletHolder holderDefault = new ServletHolder("default", DefaultServlet.class);
+        holderDefault.setInitParameter("resourceBase", baseUri.toASCIIString());
+        holderDefault.setInitParameter("dirAllowed", "true");
+        return holderDefault;
+    }
+
 }
